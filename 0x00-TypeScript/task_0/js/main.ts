dc4cb0c5edf053568af4b2be048cdec683b41009
DiegoCol93 <dfelo93@gmail.com>
2022-02-02 00:40:07 -0500

    |- ADDED -| Task 0 solution.

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -5,30 +5,34 @@ interface Student {
     location: string;
 }
 
-let studentsList: Array<Student>;
-
 let student1: Student = {
-    firstName: "Joe", lastName: "Doe", age: 18, location: "World1"
+    firstName: "Joe",
+    lastName: "Doe",
+    age: 18,
+    location: "World1"
 };
 let student2: Student = {
-    firstName: "Doe", lastName: "Joe", age: 19, location: "World2"
+    firstName: "Doe",
+    lastName: "Joe",
+    age: 19,
+    location: "World2"
 };
 
-studentsList = [student1, student2];
+let studentsList: Student[] = [student1, student2];
 
 let table = document.createElement('table');
 document.body.appendChild(table);
 
-studentsList.forEach(() => {
+studentsList.forEach(student => {
     let newRow = document.createElement('tr');
+    let cell = document.createElement('td');
 
-    let data = document.createElement('td');
-    data.innerHTML = student1.lastName;
-    newRow.appendChild(data);
+    cell.innerHTML = student.lastName;
+    newRow.appendChild(cell);
 
-    data = document.createElement('td');
-    data.innerHTML = student2.lastName;
-    newRow.appendChild(data);
+    cell.innerHTML = student.location;
+    newRow.appendChild(cell);
 
     table.appendChild(newRow);
 });
+
