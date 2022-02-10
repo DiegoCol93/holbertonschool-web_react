d795f64b063ed708908e966ff0d1bddcf69d700d
DiegoCol93 <dfelo93@gmail.com>
2022-02-10 00:22:12 -0500

    |- ADDED -| Task 1. Embedding expressions, functions.

---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+function getFullYear() {
+  return new Date().getFullYear();
+}
+
+function getFooterCopy(isIndex) {
+  if (isIndex) {
+	return 'Holberton School';
+  } else {
+	return 'Holberton School main dashboard';
+  }
+}
