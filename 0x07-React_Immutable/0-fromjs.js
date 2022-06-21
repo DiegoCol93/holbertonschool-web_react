c8f0cf2ae043eb56c63a97e0b0814d6e7d7737a8
“DiegoCol93” <dfelo93@gmail.com>
2022-06-21 18:18:20 -0500

    REMOVED comments.

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -3,12 +3,3 @@ import { fromJS } from 'immutable';
 export function getImmutableObject(object) {
   return fromJS(object);
 }
-
-// console.log(getImmutableObject(
-//   {
-//      fear: true,
-//      smell: -1033575916.9145899,
-//      wall: false,
-//      thing: -914767132
-//   }
-// ));
