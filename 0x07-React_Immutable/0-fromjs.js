155046ec856c00ac7145678620f434b88823d3f3
“DiegoCol93” <dfelo93@gmail.com>
2022-06-21 19:49:03 -0500

    ADDED const declaration.

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,8 +1,6 @@
 // Returns an immutable object
 import { fromJS } from 'immutable';
-
 const getImmutableObject = function (object) {
   return fromJS(object);
 }
-
 export default getImmutableObject;
